bed0f2b64e5b30ca4ec4dd9a9e9c6672151f0148
Akbar Kurnia Hernadi <akhernadi@gmail.com>
2021-12-07 22:32:22 +0700

    add app.js, authentication, errorhandler, userRouter (#2)

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,4 +1,24 @@
-/**
- * reserved for initialization.
- * remove this comment later.
- */
+const errorHandler = (err, req, res, next) => {
+  let code = 500;
+  let msg = 'Internal server error';
+
+  if (
+    err.name === 'SequelizeValidationError' ||
+    err.name === 'SequelizeUniqueConstraintError'
+  ) {
+    code = 400;
+    msg = err.errors[0].message;
+  } else if (err.name === 'not_authenticated') {
+    code = 401;
+    msg = 'Login first!';
+  } else if (err.name === 'JsonWebTokenError' || err.name === 'invalid_token') {
+    code = 401;
+    msg = 'Invalid token';
+  }
+
+  res.status(code).json({
+    message: msg,
+  });
+};
+
+module.exports = errorHandler;
