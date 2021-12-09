b5d0f00a6c247a63d3102b0bece509b251da627f
Giffari Yusrul K <86210172+Giffari17@users.noreply.github.com>
2021-12-09 23:22:29 +0700

    Giffari Yusrul K (#19)
    * Initialization model, end point, and controller for privilege and role
    
    * Privilege and role table has not completed yet
    
    * Added Privilege, role, and user-group feature, but has not tested yet
    
    * History has been created successfully with proper response

--- server/controllers/userGroups.js
+++ server/controllers/userGroups.js
@@ -1,61 +1,110 @@
-const { User_Group } = require('../models');
+const { Role, Privilege, User_Group } = require('../models');
 
 class UserGroupsController {
-  static async createUserGroup(req, res, next) {
-    try {
-      const { role_id, privilege_id } = req.body;
-      await User_Group.create({ role_id, privilege_id });
-      res.status(201).json({ message: 'User Group has been added' });
-    } catch (error) {
-      next(error);
+    static async createUserGroup(req, res, next) {
+        try {
+            const { role_id, privilege_id } = req.body;
+            const roleFound = await Role.findByPk(role_id);
+            const privileFound = await Privilege.findByPk(privilege_id);
+            if(!roleFound) {
+                throw { name: 'Role is not found' };
+            }
+            if(!privileFound) {
+                throw { name: 'Privilege is not found' };
+            }
+            const resp = await User_Group.create({ role_id, privilege_id });
+            const payload = {
+                entity_name: 'User_Group',
+                entity_id: resp.id,
+                user_id: req.user.id
+              };
+              const isHistoryCreated = await newHistory('createUserGroup', payload);
+              if(!isHistoryCreated) {
+                throw { name: 'Fail create history' };
+              }
+            res.status(201).json({ message: 'User Group has been added' });
+        } catch (error) {
+            res.status(500).json(error)
+            next(error);
+        } 
     }
-  }
-  static async getAllUserGroups(req, res, next) {
-    try {
-      const resp = await User_Group.findAll();
-      res.status(200).json(resp);
-    } catch (error) {
-      next(error);
+    static async getAllUserGroups(req, res, next) {
+        try {
+            const resp = await User_Group.findAll({ attributes: { 
+                exclude: ['createdAt', 'updatedAt'] } 
+            });
+            if(!resp.length) {
+                res.status(200).json({ message: 'Data is not found' });
+            } else {
+                res.status(200).json(resp);
+            }  
+        } catch (error) {
+            next(error);
+        }
     }
-  }
-  static async getUserGroup(req, res, next) {
-    try {
-      const { id } = req.params;
-      const resp = await User_Group.findByPk(id);
-      if (!resp) {
-        throw { name: 'userGroup_not_found' };
-      }
-      res.status(200).json(resp);
-    } catch (error) {
-      next(error);
+    static async getUserGroup(req, res, next) {
+        try {
+            const { id } = req.params;
+            const resp = await User_Group.findOne({ where: { id }, attributes: { 
+                exclude: ['createdAt', 'updatedAt'] } 
+            });
+            if(!resp) {
+                throw { name: 'Data is not found' };
+            }
+            res.status(200).json(resp);
+        } catch (error) {
+            next(error);
+        }
     }
-  }
-  static async updateUserGroup(req, res, next) {
-    try {
-      const { id } = req.params;
-      const { role_id, privilege_id } = req.body;
-      const foundUserGroup = await User_Group.findByPk(id);
-      if (!foundUserGroup) {
-        throw { name: 'userGroup_not_found' };
-      }
-      await User_Group.update(id, { role_id, privilege_id });
-      res.status(200).json({ message: 'User Group has been added' });
-    } catch (error) {
-      next(error);
+    static async updateUserGroup(req, res, next) {
+        try {
+            const { id } = req.params;
+            const { role_id, privilege_id } = req.body;
+            const foundUserGroup = await User_Group.findByPk(id);
+            if(!foundUserGroup) {
+                throw { name: 'Data is not found' };
+            }
+            const resp = await User_Group.update({ role_id, privilege_id }, { where: { id }, returning: true });
+            if(!resp[1]) {
+                res.status(200).json({ message: 'Everything is already uptodated' });
+            } else {
+                const payload = {
+                    entity_name: 'User_Group',
+                    entity_id: resp[1][0].id,
+                    user_id: req.user.id
+                };
+                const isHistoryCreated = await newHistory('updateUserGroup', payload);
+                if(!isHistoryCreated) {
+                    throw { name: 'Fail create history' };
+                }
+            res.status(200).json({ message: `User Group with id ${id} has been updated` })
+            }
+        } catch (error) {
+            next(error);
+        }
     }
-  }
-  static async deleteUserGroup(req, res, next) {
-    try {
-      const { id } = req.params;
-      const foundUserGroup = await User_Group.findByPk(id);
-      if (!foundUserGroup) {
-        throw { name: 'userGroup_not_found' };
-      }
-      await User_Group.destroy({ where: { id } });
-    } catch (error) {
-      next(error);
+    static async deleteUserGroup(req, res, next) {
+        try {
+            const { id } = req.params;
+            const foundUserGroup = await User_Group.findByPk(id);
+            if(!foundUserGroup) {
+                throw { name: 'Data is not found' };
+            }
+            await User_Group.destroy({ where: { id } });
+            const payload = {
+                entity_name: 'User_Group',
+                entity_id: foundUserGroup.id,
+                user_id: req.user.id
+            };
+            const isHistoryCreated = await newHistory('deleteUserGroup', payload);
+            if(!isHistoryCreated) {
+                throw { name: 'Fail create history' };
+            }
+            res.status(200).json({ message: `User Group with id ${id} has been deleted` });
+        } catch (error) {
+            next(error);
+        }
     }
-  }
 }
 
 module.exports = UserGroupsController;
