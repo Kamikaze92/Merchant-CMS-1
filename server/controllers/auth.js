0bd0cab394bfcc88968ac6b7b772891271f1fd33
Akbar Kurnia Hernadi <akhernadi@gmail.com>
2021-12-09 22:41:17 +0700

    update model merchant (#18)
    * add app.js, authentication, errorhandler, userRouter
    
    * fix userRouter, category router
    
    * update merge
    
    * update merge
    
    * add error handler for all controller
    
    * Update app.js
    
    * Update userRouter.js
    
    * Update index.js
    
    * usegroup
    
    * model merchant
    
    Co-authored-by: Mochamad Saepul M <numusai@gmail.com>

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,9 +2,10 @@ const {comparePassword} = require('../helpers/bcrypt')
 const {jwtSign, signPasswordLink} = require('../helpers/jwt')
 const {transporter, mailOtp} = require('../helpers/nodemailer')
 
+
 module.exports = class AuthController {
   // your code goes here
-  static async userLogin (req,res,next) {
+  static async userLogin(req, res, next) {
     try {
         const {email, password} = req.body
         let response = await User.findOne({
@@ -33,7 +34,7 @@ module.exports = class AuthController {
     }
   }
 
-  static async userRegister(req,res,next) {
+  static async userRegister(req, res, next) {
     try {
         const {nama_lengkap, email, no_hp, password, role_id, verificator_id} = req.body
         //validation here 
@@ -56,8 +57,11 @@ module.exports = class AuthController {
           }
         }
       }
-      catch (err) {
-        next(err)
+      res.status(201).json({
+        message: `Success registered ${response.email}`,
+      });
+    } catch (err) {
+      next(err);
     }
   }
 
---
+++ server/migrations/20211209151902-create-merchant.js
@@ -0,0 +1,62 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Merchants', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      nama_pengampuh: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      post_code: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      province: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      subcategory_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'SubCategories',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      parent_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Merchants');
+  }
+};
