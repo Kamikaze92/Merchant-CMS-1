7a9881cb521624a3ec464a642137c76a89b48817
Mochamad Saepul M <numusai@gmail.com>
2021-12-09 22:25:56 +0700

    Merge from raihanr1 (#17)
    * removed unused code
    
    * add migration user
    
    * add model user
    
    * insert user table, category table, and subcategory
    
    * add validation for user model
    
    * add migration for category table and sub category table
    
    * create category and subcategory models
    
    Co-authored-by: raihanr1 <raihanrobbani123@gmail.com>

---
+++ server/migrations/20211208015228-create-user.js
@@ -0,0 +1,69 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      fullName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      verifiedAt: {
+        type: Sequelize.DATE,
+      },
+      mobilePhone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      RoleId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Roles",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      approvedBy: {
+        type: Sequelize.STRING,
+      },
+      approvedAt: {
+        type: Sequelize.DATE,
+      },
+      VerficatorId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Verificators",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Users");
+  },
+};
