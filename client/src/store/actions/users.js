0936a948e3649e0663633c61f077a3a172f74571
Mochamad Saepul Millah <numusai@gmail.com>
2021-12-12 04:35:08 +0700

    [WIP] user creation on client

--- client/src/store/actions/users.js
+++ client/src/store/actions/users.js
@@ -32,3 +32,34 @@ export const setUsers = () => async (dispatch) => {
     });
   };
 }
+
+export const createUser = (payload) => async (dispatch) => {
+  dispatch({
+    type: USER_LOADING,
+    payload: true,
+  });
+  try {
+    const response = await axios({
+      url: `${process.env.REACT_APP_BASE_URL}/users`, 
+      method: 'POST',
+      headers: {
+        // access_token: localStorage.getItem('access_token'),
+        'Content-Type': 'application/json',
+      },
+      data: payload,
+    });
+    console.log(response);
+    if (response.status === 201) {
+      // dispatch({
+      //   type: CREATE_USER_SUCCESS,
+      //   payload: response.data,
+      // });
+    }
+  } catch (error) {
+    console.log(error.response);
+    // dispatch({
+    //   type: USER_ERROR,
+    //   payload: error,
+    // });
+  }
+}
