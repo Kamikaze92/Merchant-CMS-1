166b27d988ee98fd1444a6398723b6bcfd0283fb
brorp <ryan.feb28@gmail.com>
2021-12-11 20:23:41 +0700

    move css folder

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ function App() {
   // <div className="App">{/* <RegisterMerchant /> */}</div>;
   return (
     <>
+      <OtpInput />
       <Sidebar />
       <Routes>
         <Route path="/category" element={<Category />}></Route>
