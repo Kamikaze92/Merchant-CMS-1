3e443bff2ea370493b1ec9e40eb1036bb28cdba5
raihanr1 <raihanrobbani123@gmail.com>
2021-12-11 12:15:38 +0700

    require express

--- server/routes/categoryRouter.js
+++ server/routes/categoryRouter.js
@@ -1,3 +1,4 @@
+const express = require("express");
 const router = express.Router();
 const Controller = require("../controllers/category");
 const errorHandle = require("../middlewares/errorHandler");
