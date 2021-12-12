f74977d3c88e93a086f3c8cc6d17d0739ccfb285
Giffari17 <giffariyusrul98@mail.ugm.ac.id>
2021-12-12 21:53:19 +0700

    Added Template For User Detail, Group List, and Group Detail

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,11 @@ import { Routes, Route, Link } from "react-router-dom";
 import CategoryNonTenant from "./Pages/CategoryNonTenantPage";
 import CategoryDetail from "./Pages/CategoryDetailPage";
 import CategoryTenant from "./Pages/CategoryTenantPage";
+import UserDetail from "./Pages/UserDetailPage";
 import ListGroup from "./Pages/ListGroupPage";
 import GroupDetail from "./Pages/GroupDetailPage";
 import Users from "./Pages/Users";
 import Sidebar from "./Components/Sidebar";
-
 import OtpInput from "./Pages/OtpInput";
 import CheckStatus from "./Pages/CheckStatus";
 import RegisterMerchant from "./Pages/RegisterMerchant";
@@ -25,15 +25,18 @@ function App() {
       <RegisterMerchant />
       <RegisterVerificator /> */}
       {/* <ActiveMerchant /> */}
-      <MerchantDetail />
+      {/* <MerchantDetail /> */}
       <Routes>
         <Route path="/register-merchant" element={<RegisterMerchant />}></Route>
         <Route path="/register-verifier" element={<RegisterVerificator />}></Route>
-        <Route path="/category" element={<Category />}></Route>
+        <Route path="/category" element={<CategoryNonTenant />}></Route>
         <Route path="/category/detail" element={<CategoryDetail />}></Route>
         <Route path="/category/tenant" element={<CategoryTenant />}></Route>
+
+        <Route path="/user-detail" element={<UserDetail />}></Route>
         <Route path="/group-list" element={<ListGroup />}></Route>
         <Route path="/group-detail" element={<GroupDetail />}></Route>
+
         <Route path="/users" element={<Users />}></Route>
       </Routes>
       <div className="App">
