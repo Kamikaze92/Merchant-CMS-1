fb8f858bf9524309d81842b9f58272a1849d2084
raihanr1 <raihanrobbani123@gmail.com>
2021-12-13 01:47:48 +0700

    edit path route, add react bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import CategoryDetail from "./Pages/CategoryDetailPage";
 import CategoryTenant from "./Pages/CategoryTenantPage";
 import Users from "./Pages/Users";
 import Sidebar from "./Components/Sidebar";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import OtpInput from "./Pages/OtpInput";
 import CheckStatus from "./Pages/CheckStatus";
 import RegisterMerchant from "./Pages/RegisterMerchant";
@@ -18,10 +18,10 @@ function App() {
       <OtpInput />
       <RegisterMerchant />
       <RegisterVerificator /> */}
-      <ActiveMerchant />
+      {/* <ActiveMerchant /> */}
       <Routes>
         <Route path="/category" element={<CategoryNonTenant />}></Route>
-        <Route path="/category/detail" element={<CategoryDetail />}></Route>
+        <Route path="/category/:id" element={<CategoryDetail />}></Route>
         <Route path="/category/tenant" element={<CategoryTenant />}></Route>
         <Route path="/users" element={<Users />}></Route>
       </Routes>
