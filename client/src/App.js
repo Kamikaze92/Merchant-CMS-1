eb5370a8b52620ca1147b72f9e9c7f04ce929717
Farhad Yanuar <89724082+Kamikaze92@users.noreply.github.com>
2021-12-13 13:59:18 +0700

    Add route Guard Access Token

--- client/src/App.js
+++ client/src/App.js
@@ -17,40 +17,35 @@ import ActiveMerchantDetail from "./Pages/ActiveMerchantDetail";
 import ErrorPage from "./Pages/ErrorPage";
 import ListGroupDetail from "./Pages/ListGroupDetail";
 import ChangePassword from "./Pages/ChangePassword";
-
+import { RequireAuth } from './Components/RequireAuth';
+import Dashboard from './Pages/Dashboard';
 function App() {
   return (
     <>
       <Routes>
-        {/* auth */}
-        <Route
-          path="/register-verifier"
-          element={<RegisterVerificator />}
-        ></Route>
+        {/* auth Router Guard*/}
+        <Route path="/" element={<RequireAuth><Dashboard/></RequireAuth>}>
+          {/* Table utama */}
+          <Route path="/merchants" element={<MerchantApproval />}></Route>
+          <Route path="/verifiers" element={<UserVerifierApproval />}></Route>
+          <Route path="/active-merchants" element={<ActiveMerchant />}></Route>
+          <Route path="/active-merchants/detail" element={<ActiveMerchantDetail />}></Route>
+
+          {/* users, usergroup, category */}
+          <Route path="/users" element={<Users />}></Route>
+          <Route path="/users/detail" element={<UserDetail />}></Route>
+          <Route path="/group-list" element={<ListGroup />}></Route>
+          <Route path="/group-list/detail" element={<ListGroupDetail />}></Route>
+          <Route path="/change-password" element={<ChangePassword />}></Route>
+          <Route path="/categories" element={<CategoryNonTenant />}></Route>
+          <Route path="/categories/:id" element={<CategoryNonTenantDetail />}></Route>
+          <Route path="/categories/tenant" element={<CategoryTenant />}></Route>
+        </Route>
+        <Route path="/register-verifier" element={<RegisterVerificator />}></Route>
         <Route path="/register-merchant" element={<RegisterMerchant />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/otp-verification" element={<OtpInput />}></Route>
         <Route path="/check-status" element={<CheckStatus />}></Route>
-
-        {/* Table utama */}
-        <Route path="/merchants" element={<MerchantApproval />}></Route>
-        <Route path="/verifiers" element={<UserVerifierApproval />}></Route>
-        <Route path="/active-merchants" element={<ActiveMerchant />}></Route>
-        <Route
-          path="/active-merchants/detail"
-          element={<ActiveMerchantDetail />}
-        ></Route>
-
-        {/* users, usergroup, category */}
-        <Route path="/users" element={<Users />}></Route>
-        <Route path="/users/detail" element={<UserDetail />}></Route>
-        <Route path="/group-list" element={<ListGroup />}></Route>
-        <Route path="/group-list/detail" element={<ListGroupDetail />}></Route>
-        <Route path="/change-password" element={<ChangePassword />}></Route>
-        <Route path="/categories" element={<CategoryNonTenant />}></Route>
-        <Route path="/categories/:id" element={<CategoryNonTenantDetail />}></Route>
-        <Route path="/categories/tenant" element={<CategoryTenant />}></Route>
-
         <Route path="/errors" element={<ErrorPage />}></Route>
       </Routes>
       <div className="App">
