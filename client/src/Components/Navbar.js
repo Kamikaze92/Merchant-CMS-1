30349379c06b1776b548b1ddc52e740d7976baeb
raihanr1 <raihanrobbani123@gmail.com>
2021-12-14 07:13:57 +0700

    edit config with link deploy, navbar

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -2,11 +2,9 @@ import navbarImg from '../assets/images/Setting.svg';
 export default function Navbar(){
     return (
         <>
-        <div className="d-flex flex-row-reverse p-2">
-            <div className="d-flex flex-row p-2">
+        <div className="d-flex flex-row-reverse p-2" style={{height: '48px', backgroundColor:'#FFFFFF'}}>
             <img className="img-fluid" src={ navbarImg } style={{width: 30, height: 30, marginRight: 10}}/>
             <p>Satgas</p>
-            </div>
         </div>
         </>
     )
